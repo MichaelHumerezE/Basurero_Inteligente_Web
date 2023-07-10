9743474d7e7b32606e5407e26ffc83ca71e1ba8e
Rister Arce <yasirarce70@gmail.com>
2023-07-09 20:03:41 -0400

    Cambios de ruta en la API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\AuthChoferController;
+use App\Http\Controllers\API\AuthChoferController;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\Api\ChoferController;
+use App\Http\Controllers\API\ChoferController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
